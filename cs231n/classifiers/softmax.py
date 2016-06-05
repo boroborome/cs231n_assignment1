bf1d3d3600f58a63253fe522360fa89ec46507d5
Yang Bo <atryyang@thoughtworks.com>
2016-06-06 00:06:58 +0800

    svm and softmax are done

--- cs231n/classifiers/softmax.py
+++ cs231n/classifiers/softmax.py
@@ -24,39 +24,29 @@ def softmax_loss_naive(W, X, y, reg):
   dW = np.zeros_like(W)
 
   #############################################################################
-  # TODO: Compute the softmax loss and its gradient using explicit loops.     #
+  # DONE: Compute the softmax loss and its gradient using explicit loops.     #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  num_train = X.shape[0]
-  num_pixel = X.shape[1]
+  num_trains = X.shape[0]
+  num_pixels = X.shape[1]
   num_classes = W.shape[1]
-  for i in range(num_train):
+  for i in range(num_trains):
     correct_label = y[i]
     scores = X[i].dot(W)
     count = 0
     for j in range(num_classes):
       count += np.exp(scores[j])
     loss += -np.log(np.exp(scores[correct_label]) / count)
-#    
-#    dlog = -1 / np.exp(scores[correct_label]) * count
-#    
-#    dcorrect = -1 / np.exp(scores[correct_label])
-#    dcorrect_exp = -1
-#                                              
-#    dW[:, correct_label] += -X[i]
-#    for j in range(num_classes):
-#      dj = -count
-#      dj_reverse = count / np.exp(scores[j] * 2)
-#      dj_exp = count / np.exp(scores[j])
-#      dW[:, j] += count / np.exp(scores[j]) * X[i]
+    dW[:, correct_label] += -X[i]
+    for j in range(num_classes):
+      dW[:, j] += X[i] / count * np.exp(scores[j])
         
-  loss /= num_train
-  #dW /= num_train
+  loss /= num_trains
+  dW /= num_trains
   loss += 0.5 * reg * np.sum(W * W)      
-  #dW += reg * W
-
+  dW += reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -70,17 +60,22 @@ def softmax_loss_vectorized(W, X, y, reg):
 
   Inputs and outputs are the same as softmax_loss_naive.
   """
-  # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
 
   #############################################################################
-  # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
+  # DONE: Compute the softmax loss and its gradient using no explicit loops.  #
   # Store the loss in loss and the gradient in dW. If you are not careful     #
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_trains = X.shape[0]
+  num_pixels = X.shape[1]
+  num_classes = W.shape[1]
+  scores = X.dot(W)
+  total_exp = np.sum(np.exp(scores), 1)
+  loss = -np.sum(np.log(np.exp(scores[range(num_trains),y]) / total_exp)) / num_trains + 0.5 * reg * np.sum(W * W)
+  numerator_dW = np.bincount((y.reshape(num_trains, 1) + (np.arange(num_pixels) * num_classes).reshape(1, num_pixels)).reshape(num_trains * num_pixels), -X.reshape(num_trains * num_pixels)).reshape(num_pixels, num_classes)
+  denominator_dW = X.T.dot(np.exp(scores) / total_exp.reshape(num_trains, 1))
+  dW = (numerator_dW + denominator_dW) / num_trains + reg * W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
